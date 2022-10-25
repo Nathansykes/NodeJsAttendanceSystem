274c33e01983ba8f4795ac01db2386f8192023c4
Ben Jefferson <b9021400@hallam.shu.ac.uk>
2022-10-25 11:34:55 +0100

    Setup controller for user

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const Users = db.users;
+ 
+// Create and Save a new User
+exports.create = (req, res) => {
+ 
+};
+ 
+// Retrieve all Users from the database.
+exports.findAll = (req, res) => {
+ 
+};
+ 
+// Find a single User with an id
+exports.findOne = (req, res) => {
+ 
+};
+ 
+// Update a User by the id in the request
+exports.update = (req, res) => {
+ 
+};
+ 
+// Delete a User with the specified id in the request
+exports.delete = (req, res) => {
+ 
+};
+ 
+// Delete all Users from the database.
+exports.deleteAll = (req, res) => {
+ 
+};
--- backend/models/index.js
+++ backend/models/index.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.animals = require("./user.model.js")(mongoose);
+db.users = require("./user.model.js")(mongoose);
  
 module.exports = db;
