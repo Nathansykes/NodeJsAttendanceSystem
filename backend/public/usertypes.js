f74340c43d25fa4dea4755829ea74ef8c7972c63
Nathansykes <nathan.sykes1@hotmail.com>
2022-11-07 14:13:21 +0000

    Passing data from child component, adding UserType module

---
+++ backend/public/usertypes.js
@@ -0,0 +1,9 @@
+const UserTypes = {
+    All: {id: 0, name: "All"},
+    Student: { id: 1, name: "Student" },
+    CourseLeader: { id: 2, name: "CourseLeader" },
+    ModuleLeader: { id: 3, name: "ModuleLeader" },
+    AcademicAdvisor: { id: 4, name: "AcademicAdvisor" },
+    Tutor: { id: 5, name: "Tutor" }    
+}
+module.exports = UserTypes;
