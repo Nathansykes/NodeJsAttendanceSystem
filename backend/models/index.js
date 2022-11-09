62390373a50cc445e4afc143c24b4d672c7d2678
Ben Jefferson <b9021400@hallam.shu.ac.uk>
2022-11-09 17:06:06 +0000

    Connection string as parameter. Mocha produces test results.

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,24 @@
-const dbConfig = require("../config/db.config.js");
+const myArgs = process.argv;
+
+const connectionIndex = myArgs.indexOf('--connection');
+const connectionString = myArgs[connectionIndex + 1];
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
- 
+
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+
+if (connectionString) 
+{
+    db.url = connectionString;
+}
+else 
+{
+    const dbConfig = require("../config/db.config.js");
+    db.url = dbConfig.url;
+}
+
 db.users = require("./user.model.js")(mongoose);
  
 module.exports = db;
