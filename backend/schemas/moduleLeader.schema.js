de5eea13729aa04a8c4edef7a5964da293693528
DedicatedRam <b9002237@my.shu.ac.uk>
2022-11-10 14:54:32 +0000

    Fixing require module.

--- backend/schemas/moduleLeader.schema.js
+++ backend/schemas/moduleLeader.schema.js
@@ -2,7 +2,7 @@
 var mongoose = require("mongoose");
 let Schema =  mongoose.Schema;
 let options = require('./user.schema.options');
-let module = require('../models/module.model');
+let Module = require('../models/module.model');
 
 let moduleLeaderSchema = new Schema(
     {
