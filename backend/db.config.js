270ff0d810027b44c54a0001d25a0dcd36b5aa89
WILLIAM SIMISTER <b8017996@my.shu.ac.uk>
2022-11-15 14:35:57 +0000

    Deleted db.config

--- backend/db.config.js
+++
@@ -1,6 +0,0 @@
-const username = "will";
-const password = "8ouaqGvYEgwJZH3L";
-const connectionString = `mongodb+srv://${username}:${password}@cluster0.j8rtm4p.mongodb.net/test`
-module.exports = {
-    url: connectionString
-   };
