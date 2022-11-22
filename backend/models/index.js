1cd1d84f924905a4c9e7e828fad1b217e8459eea
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2022-11-22 00:27:38 +0000

    setting env

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,26 +1,10 @@
-const myArgs = process.argv;
-const connectionIndex = myArgs.indexOf('--connection');
-
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-
-// index.Of will return -1 by default if no index is found
-if (connectionIndex > -1) 
-{
-    const connectionString = myArgs[connectionIndex + 1];
-    if (connectionString) 
-    {
-        db.url = connectionString;
-    }
-}
-else 
-{
-    const dbConfig = require("../config/db.config.js");
-    db.url = dbConfig.url;
-}
+db.url = process.env.CONNECTION_STRING;
+console.log(db.url);
 
 db.users = require("./user.model.js")(mongoose);
  
