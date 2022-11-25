01988d111c39295aafa60009372079cf33b0c6a4
joet2077 <b8027147@my.shu.ac.uk>
2022-11-25 12:45:28 +0000

    Started calendar population logic - access cookie

--- frontend/src/components/Timetable.vue
+++ frontend/src/components/Timetable.vue
@@ -2,6 +2,9 @@
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import httpCommonDataService from '@/services/http-common.data.service'
+
 
 export default {
   name: "app",
@@ -11,14 +14,28 @@ export default {
   data() {
     return {
       calendarOptions: {
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridWeek'
+        plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin],
+        initialView: 'timeGridWeek',
+  editable: true,
+  selectable: true, 
+  weekends: false,     
       }
     }
+  },
+  methods : {
+    populateCalendar(){
+      try{
+       var cookie = httpCommonDataService.getCookie(); //access cookie stored in browser
+       
+      }catch{
+
+      }
   }
+}
 }
 </script>
 
-<template> 
+<template>
+<body onload="populateCalendar()"></body> <!-- run 'populate calendar' function on page load  -->
 <FullCalendar :options="calendarOptions" /> 
 </template>
