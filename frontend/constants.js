454da9d3b8bf2c5ed5aae3e2b0dc2f138b1f8cd0
WILLIAM SIMISTER <b8017996@my.shu.ac.uk>
2022-11-28 11:13:39 +0000

    Permissions

---
+++ frontend/constants.js
@@ -0,0 +1,16 @@
+const actions = {
+    UPLOAD_FILE: "UPLOAD_FILE",
+    VIEW_REPORT: "VIEW_REPORT",
+    MARK_ATTENDANCE: "MARK_ATTENDANCE",
+    VIEW_TIMETABLE: "VIEW_TIMETABLE"
+  };
+
+  const roles = {
+    STUDENT: "1",
+    ACADEMIC_ADVISOR: "2",
+    TUTOR: "3",
+    MODULE_LEADER: "4",
+    COURSE_LEADER: "5",
+  };
+
+  export { actions, roles };
