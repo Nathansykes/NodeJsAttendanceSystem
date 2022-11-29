0eed691670ff10a16a12a0c5bec897a7a9efc3d8
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2022-11-29 12:49:02 +0000

    exit after x

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router';
 import './assets/all.css';
 
-
+if(process.env.MODE === 'pipelines') {
+    setTimeout(() => {
+        process.exit(0);
+    }, 20000);
+}
 
 createApp(App).use(router).mount('#app');
