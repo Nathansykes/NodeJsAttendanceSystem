9a20765488ec82207b9497c52397f223756c4ce0
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2022-11-29 12:57:49 +0000

    env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router';
 import './assets/all.css';
 
-if(process.env.MODE === 'pipelines') {
+if(process.env.VUE_APP_PIPELINES == 'pipelines') {
     setTimeout(() => {
         process.exit(0);
     }, 20000);
